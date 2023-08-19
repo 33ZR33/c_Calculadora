baaac7ccf0d9644474c36071ec4b585f88357f3a
Ronal <zr33lanor@hotmail.com>
2023-08-19 17:16:26 -0600

    Se añade calculo de exponente y raiz cuadrada, además de eliminar ceros, aproximando el resultado a dos decimales

--- calculadora.c
+++ calculadora.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 double suma(double a, double b) {
     return a + b;
@@ -13,17 +14,28 @@ double multiplicacion(double a, double b) {
     return a * b;
 }
 
-double division(double a, double b){
+double division(double a, double b) {
     if (b == 0) {
-        printf("Error: División por cero no permitido.\n");
-        return -1;
+        printf("Error: División por cero no permitida.\n");
+        return -1;  // Un valor indicador de error
     }
     return a / b;
 }
 
+double potencia(double base, double exponente) {
+    return pow(base, exponente);
+}
+
+double raizCuadrada(double a) {
+    if (a < 0) {
+        printf("Error: No es posible calcular la raíz cuadrada de un número negativo.\n");
+        return -1;  // Un valor indicador de error
+    }
+    return sqrt(a);
+}
 
 bool validarOperador(char op) {
-    return op == '+' || op == '-' || op == '*' || op == '/';
+    return op == '+' || op == '-' || op == '*' || op == '/' || op == '^' || op == 'r';
 }
 
 int main() {
@@ -32,10 +44,10 @@ int main() {
     char opcion;
 
     do {
-        printf("Introduzca la operación en formato (a + b): ");
+        printf("Introduce la operación (ejemplo: a + b): ");
         if (scanf("%lf %c %lf", &num1, &operador, &num2) != 3 || !validarOperador(operador)) {
             printf("Entrada inválida. Asegúrate de ingresar una operación válida.\n");
-            continue; // Vuelve al inicio del bucle
+            continue;
         }
 
         switch (operador) {
@@ -51,17 +63,28 @@ int main() {
             case '/':
                 resultado = division(num1, num2);
                 if (resultado == -1) {
-                    return 1;
+                    continue;
+                }
+                break;
+            case '^':
+                resultado = potencia(num1, num2);
+                break;
+            case 'r':
+                resultado = raizCuadrada(num1);
+                if (resultado == -1) {
+                    continue;
                 }
                 break;
         }
 
-        printf("Resultado: %.21f %c %.21f = %.21f\n", num1, operador, num2, resultado);
-        //Preguntar al usuario si quiere realizar otra operación
+        printf("Resultado: %.2lf %c %.2lf = %.2lf\n", num1, operador, num2, resultado);
+
         printf("¿Quieres realizar otra operación? (S/N): ");
-        scanf(" %c", &opcion); //El espacio antes de %c elimina cualquier whitespace pendiente
+        scanf(" %c", &opcion);
+
     } while (opcion == 'S' || opcion == 's');
-    
-    printf("¡Hasta luego!.\n");
+
+    printf("¡Gracias por usar la calculadora! Hasta luego.\n");
     return 0;
 }
+
